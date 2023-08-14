c4bb63c4fdbe8972eea0d0da8a9a75e4e84b4834
Akash Singh <akashsinghit2024@gmail.com>
2023-08-14 23:11:24 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -3,9 +3,12 @@ require("./db/conn");
 const express = require("express");
 const mongoose = require("mongoose");
 const users = require("./models/useSchema");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());    
 
 app.listen(8000, () => {
     console.log("server is running ");
