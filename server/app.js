3712ad0274075cc5d0a30189356d1b3eafc907c4
Akash Singh <akashsinghit2024@gmail.com>
2023-08-14 23:28:36 +0530

    router

--- server/app.js
+++ server/app.js
@@ -4,11 +4,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const users = require("./models/useSchema");
 const cors = require("cors");
+const router = require("./routes/router");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());    
+app.use(router);
 
 app.listen(8000, () => {
     console.log("server is running ");
